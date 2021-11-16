cc4d65aa1be766a705625443a9930ff941dc7c58
Adam Levoy <ajlevoy86@gmail.com>
2021-11-15 18:22:29 -0800

    updated loading state

--- src/App.js
+++ src/App.js
@@ -13,8 +13,12 @@ function App() {
   const [ count, setCount ] = useState(0);
 
   if(loading) return (
-    <div>
-      Loading...
+    <div className="wrapper">
+      <h1>🔥NFTinder🔥</h1>
+      <div className="imgWrapper">
+        Loading...
+      </div>
+      <button onClick={() => setCount(initialCount => initialCount + 1)}>Next</button>
     </div>
   )
 
