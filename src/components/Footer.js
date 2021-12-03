caca0b5827dda9292ac593afc8fa2baea333c3d9
Adam Levoy <ajlevoy86@gmail.com>
2021-12-02 18:16:39 -0800

    updated footer link target

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer>
-      <p>Made with ♥️ by <a href="https://twitter.com/difool0x">@DiFool0x</a></p>
+      <p>Made with ♥️ by <a href="https://twitter.com/difool0x" target="_blank" rel="noreferrer">@DiFool0x</a></p>
     </footer>
   )
 }
